716e5adf1532a15d59082719e3786d8a768e4636
Eddie Padin <eddie@entertainermedia.net>
2018-10-24 14:12:27 -0500

    feat: display all books from api; - add Shelf component to ListShelf; - add logic in Shelf component to loop through the Book components and return the books from api; - add logic to fetch the data for each book in the Book component;

---
+++ src/components/Shelf.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+class Shelf extends Component {
+  render() {
+    const { books } = this.props;
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Category Title</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map(book => (
+              <Book book={book} key={book.id} />
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Shelf;
