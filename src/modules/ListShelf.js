cccb66a2a880ed5c05123782d976564a972abbe9
Eddie Padin <eddie@entertainermedia.net>
2018-10-24 15:41:03 -0500

    feat: display books by shelf; - add logic to get books based shelf; - add PropTypes declaration to MyReadsContainer & ListShelf; - add static books object to MyReadsContainer state for testing; - disable componentDidMount() hook temporarily; - refactor ListShelf component to loop through shelves within component instead of inside parent;

--- src/modules/ListShelf.js
+++ src/modules/ListShelf.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import Shelf from "../components/Shelf";
 
 class ListShelf extends Component {
+  /**
+   * Query to get books by shelf
+   * @param query
+   * @returns {*}
+   */
+  getBooksByShelf = query => {
+    //first we destructure the books prop from the parent component state
+    const { books } = this.props;
+
+    //next we filter the results based on the query param passed to the function & return the queried results
+    return books.filter(b => b.shelf === query);
+  };
+
   render() {
-    const { books, shelf } = this.props;
-    // console.log(books);
+    const { shelves } = this.props;
+
     return (
-      <div className="list-books">
-        <div className="list-books-content">
-          <Shelf shelf={shelf} books={books} />
-        </div>
-        {/*//search btn*/}
-      </div>
+      <React.Fragment>
+        {shelves.map(shelf => (
+          <div className="list-books" key={shelf.id}>
+            <div className="list-books-content">
+              <Shelf shelf={shelf} books={this.getBooksByShelf(shelf.id)} />
+            </div>
+          </div>
+        ))}
+      </React.Fragment>
     );
   }
 }
+
+ListShelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  shelves: PropTypes.array.isRequired
+};
 
 export default ListShelf;
