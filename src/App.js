0d3c71a29c73a11867495d034f7a46df6e8663fd
Eddie Padin <eddie@entertainermedia.net>
2018-10-24 10:30:00 -0500

    feat: removed search handler and searchButton from App.js;

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ import Header from "./modules/Header";
 import Footer from "./modules/Footer";
 
 class App extends Component {
-  handleSearch = () => {
-    console.log('handleSearch', 'Display Search')
-  };
-    render() {
-        return (
-            <React.Fragment>
-                <Header />
-                <main className="container">
-                    <MyReadsContainer searchButton={this.handleSearch}/>
-                </main>
-                <Footer />
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <main className="container">
+          <MyReadsContainer />
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
