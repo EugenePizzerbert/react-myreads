d7a3826bb94bdb65da13fa2be9a9e9b2a916760b
Eddie Padin <eddie@entertainermedia.net>
2018-10-26 00:26:15 -0500

    refactor: move browserRouter from index to App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 import MyReadsContainer from "./MyReadsContainer";
 import Header from "./modules/Header";
 import Footer from "./modules/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <main className="container">
-          <MyReadsContainer />
-        </main>
-        <Footer />
-      </React.Fragment>
+      <BrowserRouter>
+        <div className="main-page">
+          <Header />
+          <main className="container">
+            <MyReadsContainer />
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
