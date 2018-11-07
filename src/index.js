524ff2a0905158bdf037c6b1b0aba4349295a697
Eddie Padin <eddie@entertainermedia.net>
2018-11-07 14:17:24 -0600

    refactor: fontawesome libraries; - refactor fontawesome libraries to prevent having to explicit import specific icons in each component;

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import App from "./App";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-library.add(faArrowLeft);
+library.add(fas, far, fab);
 
 ReactDOM.render(<App />, document.getElementById("root"));
